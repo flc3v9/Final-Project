aaefccd02cd527dd3bb6c7fc50e3f1aefa9b3682
flc3v9 <97908586+flc3v9@users.noreply.github.com>
2023-04-26 09:14:29 -0500

    delete stocks

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect, abort
 
 # make a Flask application object called app
@@ -32,29 +31,6 @@ def admin():
 @app.route("/reservations", methods=('GET', 'POST'))
 def reservations():
     return render_template('reservations.html')
-
-
-@app.route("/stock", methods=('GET', 'POST'))
-def stocks():
-    if request.method == 'POST':
-        # get form data
-        symbol = request.form['symbol']
-        chart_type = request.form['chart_type']
-        time_series = request.form['time_series']
-        start_date = request.form['start_date']
-        end_date = request.form['end_date']
-
-        if symbol == '':
-            flash("Symbol is requried.")
-        else:
-            # query API
-            chart = get_chart()
-            return render_template("stock.html", chart=chart)
-
-
-
-        # valuidate form data and flask error message if error
-    return render_template("stock.html")
 
 
 app.run(host="0.0.0.0")
